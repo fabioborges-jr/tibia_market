f5a872d9d9443ab194420b75757fa0d8fb28b8e1
fabioborges <fabioborgesouzajunior@gmail.com>
2022-07-29 01:57:29 -0300

    footer created

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+import styles from "./styles.module.scss"
+
+export default function Footer() {
+    return (
+        <footer className={styles.footerContainer}>
+            <p>Developed by Swift Blast</p>
+            <p>(Fábio Borges)</p>
+        </footer>
+    )
+}
