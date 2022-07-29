d2eb440456a2421cbcd2193367db42f910ab7cc7
fabioborges <fabioborgesouzajunior@gmail.com>
2022-07-29 17:04:22 -0300

    styles responsive

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import Image from "next/image";
 import SignInButton from "../SignInButton/SignInButton";
 
@@ -6,16 +7,16 @@ import styles from "./styles.module.scss"
 export default function Header() {
     return (
         <header className={styles.headerContainer}>
-            
+
             <div className={styles.container}>
-                <Image src="/images/tibiamarket.svg" alt="" width={47} height={49} />
-                
+                <img src="/images/tibiamarket.svg" alt="" width={47} height={49} />
+
                 <nav>
                     <a className={styles.active}>Home</a>
                     <a>Market</a>
                     <a>About</a>
                 </nav>
-                
+
                 <SignInButton />
             </div>
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import Image from "next/image";
 import styles from "./home.module.scss"
 
@@ -6,14 +7,14 @@ export default function Home() {
     <main className={styles.mainContainer}>
 
       <section className={styles.sectionContainer}>
-        
+
         <span>Welcome to <Image src="/images/tibiaCoins.png" alt="market tibia" width={146} height={146} />
           <h1>MarketTibia</h1>
           a marketplace located at website,<br /> provided by the community
         </span>
-        
-      <Image src="/images/market.png" alt="market tibia" width={749} height={539}/>
-     
+
+        <img src="/images/market.png" alt="market tibia" width={749} height={539} />
+
       </section>
 
     </main>
