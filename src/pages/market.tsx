b34f0302a855391f5156fbae14c5d2a8276e1774
fabioborges <fabioborgesouzajunior@gmail.com>
2022-07-29 02:47:11 -0300

    created market page and market's style

---
+++ src/pages/market.tsx
@@ -0,0 +1,9 @@
+import styles from "./market.module.scss"
+
+export default function Market() {
+    return (
+        <main className={styles.mainContainer}>
+            <h1>Hello World</h1>
+        </main>
+    )
+}
