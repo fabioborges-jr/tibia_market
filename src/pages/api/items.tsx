a7b8ebdb08e88e90eeb3629258e5709225b85d07
fabioborges <fabioborgesouzajunior@gmail.com>
2022-08-18 13:46:58 -0300

    adding the object items for each world in api

--- src/pages/api/items.tsx
+++ src/pages/api/items.tsx
@@ -1,18 +1,23 @@
 export default function items(req, res) {
+
+    function Item(world) {
+        this.world = world
+        this.item = [
+            { category: "other", name: "Tibia Coin", image: "/images/itemTibiaCoin.gif", buyPrices: [], sellPrices: [] },
+            { category: "legs", name: "Falcon Greaves", image: "/images/itemFalconGreaves.gif", buyPrices: [], sellPrices: [] },
+            { category: "weapons", name: "Soulbleeder", image: "/images/itemSoulbleeder.gif", buyPrices: [], sellPrices: [] },
+            { category: "weapons", name: "Soulshredder", image: "/images/itemSoulshredder.gif", buyPrices: [], sellPrices: [] },
+            { category: "weapons", name: "Soulmainer", image: "/images/itemSoulmainer.gif", buyPrices: [], sellPrices: [] },
+            { category: "weapons", name: "Souleater", image: "/images/itemSouleater.gif", buyPrices: [], sellPrices: [] },
+            { category: "weapons", name: "Soultainter", image: "/images/itemSoultainter.gif", buyPrices: [], sellPrices: [] },
+            { category: "weapons", name: "Soulhexer", image: "/images/itemSoulhexer.gif", buyPrices: [], sellPrices: [] }
+        ]
+    }
+
+    const worlds = ["Venebra", "Reinobra"]
+
     let data = [
-        {
-            world:"",
-            items: [
-                { category: "other", name: "Tibia Coin", image: "/images/itemTibiaCoin.gif", buyPrices: [], sellPrices: [3000, 4000] },
-                { category: "legs", name: "Falcon Greaves", image: "/images/itemFalconGreaves.gif", buyPrices: [], sellPrices: [] },
-                { category: "weapons", name: "Soulbleeder", image: "/images/itemSoulbleeder.gif", buyPrices: [], sellPrices: [] },
-                { category: "weapons", name: "Soulshredder", image: "/images/itemSoulshredder.gif", buyPrices: [], sellPrices: [] },
-                { category: "weapons", name: "Soulmainer", image: "/images/itemSoulmainer.gif", buyPrices: [], sellPrices: [] },
-                { category: "weapons", name: "Souleater", image: "/images/itemSouleater.gif", buyPrices: [], sellPrices: [] },
-                { category: "weapons", name: "Soultainter", image: "/images/itemSoultainter.gif", buyPrices: [], sellPrices: [] },
-                { category: "weapons", name: "Soulhexer", image: "/images/itemSoulhexer.gif", buyPrices: [], sellPrices: [] }
-            ]
-        }
+        worlds.map(world => new Item(world))
     ]
 
     res.json(data);
